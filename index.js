bd5789ccdda366cb9bed1be80a4f5e0f995a15dd
root <root@DESKTOP-NQEJCB5.localdomain>
2023-03-09 15:11:19 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const cats = [ "Milo", "Otis", "Garfield" ]
+
+function destructivelyAppendCat(name) {
+  cats.push(name)
+}
+
+function destructivelyPrependCat(name) {
+  cats.unshift(name)
+}
+
+function destructivelyRemoveLastCat() {
+  cats.pop()
+}
+
+function destructivelyRemoveFirstCat() {
+  cats.shift()
+}
+
+function appendCat(name) {
+  const home=[...cats,name]
+  return home
+}
+
+function  prependCat(name) {
+  const dog=[name,...cats]
+  return dog
+}
+
+function removeLastCat() {
+  const lion= cats.slice(0,-1)
+  return lion
+}
+
+function removeFirstCat() {
+  const snake= cats.slice(1)
+  return snake
+}
